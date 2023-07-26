a8c26e96101b2cc4319b7bed3b36ee1ed71282ee
user <kola89611@gmail.com>
2023-07-26 13:00:48 +0300

    User templates

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', user_home, name='home'),
+    path('', user_profile, name='user_profile'),
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
--- backend/user/views.py
+++ backend/user/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib import messages
 from .forms import UserRegisterForm, UserLoginFrom
+from send_email.models import Contact
+from django.contrib.auth.decorators import login_required
+
+
+def additional_user_models(request):
+    user_contact = Contact.objects.filter(user_id=request.user).exists()
+    if not user_contact:
+        user_contact = Contact()
+        user_contact.user = request.user
+        user_contact.subscribed_mail = False
+        user_contact.save()
+    return
 
 
 def register(request):
@@ -10,6 +22,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            additional_user_models(request)
             messages.success(request, 'You successfully registered!')
             return redirect('login')
         else:
@@ -25,8 +38,9 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            additional_user_models(request)
             messages.success(request, 'You successfully logged in!')
-            return redirect('home')
+            return redirect('user_profile')
         else:
             for error in list(form.errors.values()):
                 messages.error(request, error)
@@ -35,6 +49,20 @@ def user_login(request):
     return render(request, 'user/login.html', {'form': form})
 
 
+@login_required
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+
+@login_required
+def user_profile(request):
+    if request.user.is_authenticated:
+        contact_user = Contact.objects.filter(user=request.user).get()
+        context = {
+            'title': 'User Profile',
+            'contact_user': contact_user,
+        }
+        return render(request, 'user/user_profile.html', context=context)
+    else:
+        return redirect('login')
