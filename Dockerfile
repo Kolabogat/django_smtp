b01bfdf72ab2d5e884d268963adb4c99d5e1f832
user <kola89611@gmail.com>
2023-07-27 22:36:59 +0300

    docker-compose setup

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.10
+
+RUN apt update -y && apt upgrade -y
+
+WORKDIR /app
+
+RUN /usr/local/bin/python -m pip install --upgrade pip
+COPY ./requirements.txt /app/
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./backend /app
+WORKDIR /app
+
+ADD start.sh /
+RUN chmod +x /start.sh
+
+CMD ["/start.sh"]
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL
+python manage.py runserver 0.0.0.0:8000
