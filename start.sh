48dc80da98416f1489737194d7d32575b15c482d
user <kola89611@gmail.com>
2023-07-28 11:22:07 +0300

    Add static command and directory

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+python manage.py collectstatic
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL
