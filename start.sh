9655611304d1aad20eb7f1521a73c0a83722fdc8
user <kola89611@gmail.com>
2023-07-28 11:31:32 +0300

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python manage.py collectstatic
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL
 python manage.py runserver 0.0.0.0:8000
